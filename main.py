4c9df8942fc4d78601325425d566f512337ecc3e
omer-studio <omerstudio@gmail.com>
2025-05-29 08:10:34 +0700

    Update main.py ניסיון למחוק

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("היי")
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, I'm alive!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
